a57467a642e3711ebc03e1aab62a345110fcc772
ITFakeFace <dthung6604@gmail.com>
2024-06-24 11:46:37 +0700

    Init Project

---
+++ SchoolManagementSystem/SchoolManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+    }
+}
